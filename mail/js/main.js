5ab7ad8c212b38759058484c9ea3203a06e5fa37
pacimarco <m.paci1987@gmail.com>
2022-07-07 14:43:41 +0200

    fine esercizio mail

--- mail/js/main.js
+++ mail/js/main.js
@@ -5,9 +5,13 @@ for (let mail = 0; mail < mail_list.length; mail++) {
     
     let mail_client = prompt ('Inserisci la tua mail') ;
 
-    if (mail_list[mail]<=mail_client<=mail_list[mail]) {
+    if (mail_client == mail_list[mail]) {
     alert ('accesso autorizzato');
     }else {
-        alert ('accesso negato, email non registrata!');
+        alert ('accesso negato, email non registrata, riprova');
     }
+        
 }
+    // console.log (mail_list[mail]);
+
+
