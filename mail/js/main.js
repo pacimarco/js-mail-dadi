c092acc92a4ecb3de718791ad328074f4d8d07cd
pacimarco <m.paci1987@gmail.com>
2022-07-07 14:34:09 +0200

    prove esercizio mail

---
+++ mail/js/main.js
@@ -0,0 +1,13 @@
+const mail_list = ['mariorossi@info.it','ugorossi@info.it','dariorossi@info.it','davidrossi@info.it'];
+
+for (let mail = 0; mail < mail_list.length; mail++) {
+    console.log (mail_list[mail]);
+    
+    let mail_client = prompt ('Inserisci la tua mail') ;
+
+    if (mail_list[mail]<=mail_client<=mail_list[mail]) {
+    alert ('accesso autorizzato');
+    }else {
+        alert ('accesso negato, email non registrata!');
+    }
+}
