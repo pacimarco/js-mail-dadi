9fef839d6d73195ed415e526473f474a6d2b421f
pacimarco <m.paci1987@gmail.com>
2022-07-08 10:31:11 +0200

    false e true

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,17 +1,23 @@
 const mail_list = ['mariorossi@info.it','ugorossi@info.it','dariorossi@info.it','davidrossi@info.it'];
 
+let mail_client = prompt ('Inserisci la tua mail') ;
+
+let accesso = false;
+
 for (let mail = 0; mail < mail_list.length; mail++) {
     console.log (mail_list[mail]);
     
-    let mail_client = prompt ('Inserisci la tua mail') ;
+    
 
     if (mail_client == mail_list[mail]) {
     alert ('accesso autorizzato');
-    }else {
-        alert ('accesso negato, email non registrata, riprova');
-    }
+    accesso = true;
         
 }
     // console.log (mail_list[mail]);
 
-
+if (accesso == true) {
+    alert ('accesso autorizzato, benvenuto!');
+} else {
+    alert ('accesso negato,la tua mail non è presente nella lista, riprova.');
+}
